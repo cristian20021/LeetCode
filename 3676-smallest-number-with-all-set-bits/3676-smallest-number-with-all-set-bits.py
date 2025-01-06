add46316bdb12f10ffe319b228f9170d1fca44f2
cristian20021 <116003823+cristian20021@users.noreply.github.com>
2025-01-06 13:48:19 +0200

    Time: 0 ms (100%), Space: 12.4 MB (8.74%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.py
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.py
@@ -1,15 +1,4 @@
 class Solution(object):
-    def check(self,n):
-        n_str = str(n)
-        for i in n_str:
-            if i == "0":
-                return False
-        return True
 
     def smallestNumber(self, n):
-        while True:
-            if self.check(format(n ,"b")) == True:
-                return n
-                break
-            else:
-                n+=1
+        return (1 << n.bit_length()) - 1
