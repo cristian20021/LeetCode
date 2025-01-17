dada0251b4c7cb9015da608e4cf1537baba5c907
cristian20021 <116003823+cristian20021@users.noreply.github.com>
2025-01-17 17:45:50 +0200

    Time: 0 ms (100%), Space: 12.4 MB (42.57%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.py
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def hammingDistance(self, x, y):
-        x_b = str(format(x ,"0256b"))
-        y_b = str(format(y ,"0256b"))
+        x_b = str(format(x ,"0128b"))
+        y_b = str(format(y ,"0128b"))
         count = 0
         for i in range(len(x_b)):
             if x_b[i]!=y_b[i]:
