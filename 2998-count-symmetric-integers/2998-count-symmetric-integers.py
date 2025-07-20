af5405cbbf1a064ab26994807fd3678d0df4f098
cristian20021 <116003823+cristian20021@users.noreply.github.com>
2025-07-20 11:18:43 +0300

    Time: 1365 ms (65.73%), Space: 12.8 MB (21.13%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.py
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.py
@@ -4,10 +4,7 @@ class Solution(object):
         for i in range(low,high+1):
             if (i>9 and i<100) or (i>999 and i<10000):
                 n = int(len(str(i)) / 2)
-                print(n)
                 localLstInt = [int(x) for x in str(i)]
-                # print(localLstInt[:2])
-                if sum(localLstInt[:n]) == sum(localLstInt[n:]):
-                    # print(i)
+                if sum(localLstInt[:n]) == sum(localLstInt[n:]):        
                     count+=1
         return count
