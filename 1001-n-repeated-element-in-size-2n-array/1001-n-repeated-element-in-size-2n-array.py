c9c15c34d36a6ee8650a4fb68e2eede1611c9eda
cristian20021 <116003823+cristian20021@users.noreply.github.com>
2025-07-20 08:43:56 +0300

    Time: 0 ms (100%), Space: 18.6 MB (73.4%) - LeetHub

--- 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.py
+++ 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.py
@@ -1,11 +1,9 @@
-class Solution(object):
-    def repeatedNTimes(self, nums):
-        numsDict = {}
-        for i in nums:
-            if i not in numsDict:
-                numsDict[i] = 1
-            else:
-                numsDict[i] += 1
-        for key, value in numsDict.items():
-            if value == len(nums)/2:
-                return key 
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        
+        list1 = []
+        for i in nums :
+            if i in list1 :
+                return i
+            else :
+                list1.append(i)
