ace591e42f37b8f19ec9994542d96ce81f8521c1
cristian20021 <116003823+cristian20021@users.noreply.github.com>
2025-09-03 13:54:39 +0300

    Time: 394 ms (67.85%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,9 +1,3 @@
 # Write your MySQL query statement below
 select firstName,lastName,city,state from Person left join Address ON Person.personId = Address.personId
 
-
--- SELECT column_name(s)
--- FROM table1
--- FULL OUTER JOIN table2
--- ON table1.column_name = table2.column_name
--- WHERE condition;
