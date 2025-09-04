cac91012cebe5a298e06fb89ee389c1ea786df3d
cristian20021 <116003823+cristian20021@users.noreply.github.com>
2025-09-04 15:07:43 +0300

    Time: 2 ms (6.99%), Space: 12.5 MB (6.99%) - LeetHub

---
+++ 3830-find-closest-person/3830-find-closest-person.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def findClosest(self, x, y, z):
+        if abs(x-z)<abs(y-z):
+            return 1
+        elif abs(x-z)>abs(y-z):
+            return 2
+        return 0
