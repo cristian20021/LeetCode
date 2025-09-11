672e4eb142ee0fbd07a95dff06148c79187153b7
cristian20021 <116003823+cristian20021@users.noreply.github.com>
2025-09-11 14:51:49 +0300

    Time: 1307 ms (23.53%), Space: 17.8 MB (54.25%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.py
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.py
@@ -2,17 +2,19 @@ class Solution(object):
     def sortVowels(self, s):
         vow = ['a','e','i','o','u','A','E','I','O','U']
         local_vow = []
+        newS = ''
+        count = 0
+
         for i in s:
             if i in vow:
                 local_vow.append(i)
-        
         local_vow = sorted(local_vow)
-        newS = ''
-        count = 0
+
         for j in range(len(s)):
             if s[j] not in vow:
                 newS+=s[j]
             else:
                 newS+=local_vow[count]
                 count+=1
+                
         return newS
