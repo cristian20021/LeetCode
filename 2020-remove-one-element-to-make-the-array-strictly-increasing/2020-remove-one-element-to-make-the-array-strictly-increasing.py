d58905a0b305907efad7345a81a9fa87adabd651
cristian20021 <116003823+cristian20021@users.noreply.github.com>
2025-09-16 13:20:21 +0300

    Time: 317 ms (18.82%), Space: 12.7 MB (16.47%) - LeetHub

--- 2020-remove-one-element-to-make-the-array-strictly-increasing/2020-remove-one-element-to-make-the-array-strictly-increasing.py
+++ 2020-remove-one-element-to-make-the-array-strictly-increasing/2020-remove-one-element-to-make-the-array-strictly-increasing.py
@@ -1,19 +1,23 @@
 class Solution(object):
     def check(self,List):
+        
         for j in range(len(List)-1):
             if List[j]>=List[j+1]:
                 return False
+
         return True
             
     def canBeIncreasing(self, nums):
         count = 0
-        for i in range(len(nums)):
-            
+
+        for i in range(len(nums)):  
             newL = nums[:]
             del newL[i]
             a = self.check(newL)
             if a == True:
                 count+=1
+
         if count > 0:
             return True
+
         return False
