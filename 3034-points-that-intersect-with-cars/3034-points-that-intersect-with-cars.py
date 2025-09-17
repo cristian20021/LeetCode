c75311f27298d2a4efd1434baa4cda9381357843
cristian20021 <116003823+cristian20021@users.noreply.github.com>
2025-09-17 11:49:57 +0300

    Time: 75 ms (5.4%), Space: 12.6 MB (27.03%) - LeetHub

--- 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.py
+++ 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.py
@@ -1,6 +1,7 @@
 class Solution(object):
     def numberOfPoints(self, nums):
         toRet = []
+        
         for i in range(len(nums)):
             for j in range(nums[i][0],nums[i][1]+1):
                 if j not in toRet:
