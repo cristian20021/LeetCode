714c6770c7feb4da2ca5a27d228544dda1ede4b3
cristian20021 <116003823+cristian20021@users.noreply.github.com>
2025-09-18 15:05:06 +0300

    Time: 3 ms (98.29%), Space: 12.8 MB (79.71%) - LeetHub

---
+++ 0879-maximize-distance-to-closest-person/0879-maximize-distance-to-closest-person.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def maxDistToClosest(self, seats):
+        maxx = 0
+        j = 1
+        i = 0
+        while j<=len(seats)-1:
+            if seats[j]==1:
+                if seats[i] == 1:
+                  localMax = (j-i)/2
+                else:
+                    localMax = j
+                if localMax>maxx:
+                    maxx=localMax
+                i = j
+                j += 1
+            else:
+                j+=1  
+        if maxx == 0:
+            return j-1
+        elif seats[-1] == 0:
+            if len(seats)-i-1>maxx:
+                return len(seats)-i-1
+        return maxx
