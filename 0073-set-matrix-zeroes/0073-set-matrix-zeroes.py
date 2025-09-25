f1cfbe4db6775b01022da169426364bd71497726
cristian20021 <116003823+cristian20021@users.noreply.github.com>
2025-09-25 15:02:31 +0300

    Time: 7 ms (47.19%), Space: 13.3 MB (12.85%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def changeToZero(self,locations,matrix):
+        for z in locations:
+            for a in range(len(matrix[z[0]])):
+                matrix[z[0]][a] = 0
+        for x in locations:
+            for s in range(len(matrix)):
+                matrix[s][x[1]] = 0
+
+
+    def setZeroes(self, matrix):
+        locations = []
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if matrix[i][j] == 0:
+                    locations.append([i,j])
+        self.changeToZero(locations,matrix)
+                    
