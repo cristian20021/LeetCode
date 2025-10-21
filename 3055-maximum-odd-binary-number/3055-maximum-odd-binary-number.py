18332eef303c785adf39fc59b7aaa09f80d92941
Ciobanu Cristian <116003823+cristian20021@users.noreply.github.com>
2025-10-21 15:21:00 +0300

    Time: 0 ms (100%), Space: 12.4 MB (52.67%) - LeetHub

--- 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.py
+++ 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.py
@@ -4,10 +4,6 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        
-        zeros = s.count('0')
-        ones = s.count('1')
-        
-        return (ones-1)*'1' + zeros*'0'+'1'
+        return (s.count('1')-1)*'1' + s.count('0')*'0'+'1'
         
         
