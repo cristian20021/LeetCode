32d9894ff6d39803c9a50ba7398a4b24d5f7fc9f
Ciobanu Cristian <116003823+cristian20021@users.noreply.github.com>
2025-11-14 12:53:14 +0200

    Time: 1012 ms (44.44%), Space: 45.6 MB (77.78%) - LeetHub

---
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def rangeAddQueries(self, n, queries):
+        """
+        :type n: int
+        :type queries: List[List[int]]
+        :rtype: List[List[int]]
+        """
+
+        diff = [[0] * n for _ in range(n)]
+
+        # diff array updates
+        for row1, col1, row2, col2 in queries:
+            for i in range(row1, row2 + 1):
+                diff[i][col1] += 1
+                if col2 + 1 < n:
+                    diff[i][col2 + 1] -= 1
+
+        # prefix sum row-wise
+        for i in range(n):
+            for j in range(1, n):
+                diff[i][j] += diff[i][j - 1]
+
+        return diff
